bdb6e1ccbb52caf8192107b43bc91a209b048c1e
Sergei <mr.sergei82@mail.ru>
2021-06-27 16:19:44 +0300

    движение капли в низ и по диаганали

--- src/newGame/GameWindow.java
+++ src/newGame/GameWindow.java
@@ -1,16 +1,29 @@
 package newGame;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
     private static GameWindow game_window;
-    public static void main(String[] args){
+    private static long last_frame_time;
+    private static Image backGround;
+    private static Image game_over;
+    private static Image drop;
+    private static float drop_left = 10;
+    private static float drop_top = -100;
+    private static float drop_v = 200;
+    public static void main(String[] args) throws IOException {
+        backGround = ImageIO.read(GameWindow.class.getResourceAsStream("nebo2.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("gameover.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("kaply2.png"));
         game_window = new GameWindow();
         game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         game_window.setLocation(200, 100);
         game_window.setSize(906, 478);
         game_window.setResizable(false);
+        last_frame_time = System.nanoTime();
         GameField game_field = new GameField();
         game_window.add(game_field);
         game_window.setVisible(true);
@@ -18,7 +31,15 @@ public class GameWindow extends JFrame {
 
     }
     private static void onRepaint(Graphics g){
-        g.drawLine(10,10,300,40);
+        long current_time = System.nanoTime();
+        float delta_time =(current_time - last_frame_time) * 0.000000001f;
+        last_frame_time = current_time;
+
+        drop_top = drop_top + drop_v * delta_time/2;
+        drop_left = drop_left + drop_v * delta_time;
+        g.drawImage(backGround, 0, 0, null);
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null);
+       // g.drawImage(game_over, 280, 120, null);
 
     }
     private static class GameField extends JPanel {
@@ -26,6 +47,7 @@ public class GameWindow extends JFrame {
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
             onRepaint(g);
+            repaint();
 
         }
     }
